d1a4f5421b2b464b9c775f88fc16ea0a81f97fb6
fornost <fornost461@users.noreply.github.com>
2016-08-22 16:31:55 +0200

    dependencies

--- js/organizer.js
+++ js/organizer.js
@@ -1,1 +1,10 @@
+if (typeof loadedFiles === "undefined") { throw new Error("module required"); }
+if (!loadedFiles.hasOwnProperty("task.js")) { throw new Error("module required"); }
 
+// code begins
+
+// ...
+
+// code ends
+
+loadedFiles["organizer.js"] = true;
--- js/task.js
+++ js/task.js
@@ -1,1 +1,10 @@
+if (typeof loadedFiles === "undefined") { var loadedFiles = {}; }
+
+// code begins
+
+// ...
+
+// code ends
+
+loadedFiles["task.js"] = true;
 
