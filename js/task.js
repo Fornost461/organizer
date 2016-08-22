9dc66e37da43e67fe0bd1299bd173b2ff73dfe7e
fornost <fornost461@users.noreply.github.com>
2016-08-22 18:49:43 +0200

    stuff

--- js/task.js
+++ js/task.js
@@ -1,9 +1,38 @@
 if (typeof loadedFiles === "undefined") { var loadedFiles = {}; }
 
-// code begins
+var makeTask = (function () {
+    var nextID = 0;
 
-// ...
+    function getNextId() {
+        nextID += 1;
+        return nextID;
+    }
 
-// code ends
+    return function (desc, limitDate, priority, prerequisites) {
+        "use strict";
+        var instance = {};
+
+        // private methods
+        //~ var privateFunction = function (args) {
+            
+        //~ };
+
+        // privileged methods
+        instance.getID = function () {
+            return ID;
+        };
+
+        // private fields
+        var ID = getNextId();
+
+        // public fields
+        instance.desc = desc;
+        instance.limitDate = limitDate;
+        instance.priority = priority;
+        instance.prerequisites = prerequisites;
+
+        return instance;
+    };
+)();
 
 loadedFiles["task.js"] = true;
