d615db041638b42d216c54b939d9b58ad9169f65
fornost <fornost461@users.noreply.github.com>
2016-08-22 21:07:27 +0200

    stuff

--- js/task.js
+++ js/task.js
@@ -8,28 +8,96 @@ var makeTask = (function () {
         return nextID;
     }
 
-    return function (desc, limitDate, priority, prerequisites) {
+    return function (parent, description, limitDate, priority, prerequisites) {
         "use strict";
         var instance = {};
 
+
         // private methods
+
         //~ var privateFunction = function (args) {
             
         //~ };
 
+
         // privileged methods
+
         instance.getID = function () {
             return ID;
         };
 
+        instance.removeChild = function (child) {
+            var index = instance.indexOf(child);
+            if (index < 0) {
+                throw new Error("child not found. child: “" + child + "”. parent: “" + instance + "”");
+            }
+            else {
+                instance.splice(index, 1);
+            }
+        };
+
+        instance.addChild = function (child) {
+            instance.children.push(instance);
+        };
+
+        instance.setParent = function (newParent) {
+            if (instance.parent !== null) {
+                instance.parent.removeChild(instance);
+            }
+            instance.parent = newParent;
+            instance.parent.addChild(instance);
+        };
+
+        instance.canBeDoneNow = function () {
+            if ()
+            // TODO
+        };
+
+        instance.expired = function () {
+            // TODO
+        };
+
+
         // private fields
+
         var ID = getNextId();
 
+
         // public fields
-        instance.desc = desc;
-        instance.limitDate = limitDate;
-        instance.priority = priority;
-        instance.prerequisites = prerequisites;
+
+        instance.parent = parent;
+        instance.children = [];
+
+        if (typeof desc === "undefined" || desc === null) {
+            instance.desc = "";
+        }
+        else {
+            instance.desc = desc;
+        }
+
+
+        if (typeof limitDate === "undefined") {
+            instance.limitDate = null;
+        }
+        else {
+            instance.limitDate = limitDate;
+        }
+
+
+        if (typeof priority === "undefined" || priority === null) {
+            instance.priority = 0;
+        }
+        else {
+            instance.priority = priority;
+        }
+
+
+        if (typeof prerequisites === "undefined") {
+            instance.prerequisites = null;
+        }
+        else {
+            instance.prerequisites = prerequisites;
+        }
 
         return instance;
     };
